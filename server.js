1bd64ffbf066ca859e109fb18370e3672642be44
Geoffrey Cruz Welborn <gcruzwelborn@gmail.com>
2016-03-11 11:12:36 -0800

    more heroku tweaking

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express')
 const path = require('path')
 const app = express()
 const bodyParser = require('body-parser')
-const port = process.env.PORT || 3000
+let port = process.env.PORT || 3000
 const tinder = require('tinderjs')
 const client = new tinder.TinderClient()
 
